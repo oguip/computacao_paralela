7ebe98c98fcf0eb63264be00c5e2c0e87a107dba
Ubuntu <ubuntu@ip-172-31-58-53.ec2.internal>
2023-03-13 21:55:25 +0000

    ex3.c

--- lab01b/ex3.c
+++ lab01b/ex3.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 
 int main() {
-  int a;
-  printf("Digite o número máximo: ");
-  scanf("%d", &a);
-  for(int i = 0; i < a; i++){
-    a=-1;
-    for(int j = 0; j < a; j++){
-      printf("%d",j+1);
+    int n;
+    printf("Número ímpar: ");
+    scanf("%d", &n);
+
+    int espacos = 0;
+    for (int i = 1; i <= n; i += 2) {
+        for (int j = 0; j < espacos; j++) {
+            printf("  ");
+        }
+        espacos++;
+        for (int j = i; j <= n; j++) {
+            printf("%d ", j);
+        }
+        printf("\n");
     }
-  }
-  return 0;
+
+    return 0;
 }
+
