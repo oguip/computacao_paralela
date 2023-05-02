8585c68c8ac93a53109c20adbaae51d07566014c
Ubuntu <ubuntu@ip-172-31-86-92.ec2.internal>
2023-05-02 01:44:52 +0000

    a

--- projeto/eParaleloSemCache.c
+++ projeto/eParaleloSemCache.c
@@ -4,8 +4,8 @@
 #include <omp.h>
 #include <gmp.h>
 
-void serieTaylor(mpf_t *e, mpf_t x, int n) {
-    int thread_count = omp_get_max_threads();
+void serieTaylor(mpf_t *e, mpf_t x, int n, int thread_count) {
+    printf("\nQuantiade de threads: %d\n",thread_count);
 
     #pragma omp parallel num_threads(thread_count)
     {
@@ -54,19 +54,19 @@ void save_to_file(mpf_t e, const char *filename) {
     fclose(file);
 }
 
-int main() {
+int main(int argc, char *argv[]) {
     int n = 0;
     mpf_t e, x;
     mpf_init2(e, 16777216);
     mpf_init2(x, 16777216);
     mpf_set_ui(e, 0);
     mpf_set_ui(x, 1);
-
+    int thread_count = strtol(argv[1], NULL, 10);
     printf("Insira o n: ");
     scanf("%d", &n);
     printf("Valor de n = %d\n", n);
 
-    serieTaylor(&e, x, n);
+    serieTaylor(&e, x, n, thread_count);
 
     gmp_printf("\nResultado: Salvo");
     save_to_file(e, "resultadoParaleloSemCache.txt");
