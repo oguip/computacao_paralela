8b5ea23f50c4c2f06c201b0799abeae8a7b04a6b
Ubuntu <ubuntu@ip-172-31-86-92.ec2.internal>
2023-05-04 00:52:08 +0000

    a

--- projeto/eParaleloTeste.c
+++ projeto/eParaleloTeste.c
@@ -4,45 +4,52 @@
 #include <omp.h>
 #include <gmp.h>
 
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <gmp.h>
+
+void partial_sum(mpf_t result, int start, int end, int step) {
+    mpf_t term, inv_term;
+    mpf_init2(term, 16777216);
+    mpf_init2(inv_term, 16777216);
+    mpf_set_ui(term, 1);
+    mpf_set_ui(result, 0);
+
+    for (int i = start; i <= end; i += step) {
+        mpf_ui_div(inv_term, 1, term);
+        mpf_add(result, result, inv_term);
+        for (int j = 0; j < step; ++j) {
+            mpf_div_ui(term, term, i + j + 1);
+        }
+    }
+
+    mpf_clear(term);
+    mpf_clear(inv_term);
+}
+
 void serieTaylor(mpf_t *e, int n, int thread_count) {
-    printf("\nQuantiade de threads: %d\n",thread_count);
+    printf("\nQuantidade de threads: %d\n", thread_count);
+
+    mpf_t partial_results[2];
+    mpf_init2(partial_results[0], 16777216);
+    mpf_init2(partial_results[1], 16777216);
 
     #pragma omp parallel num_threads(thread_count)
     {
         int my_rank = omp_get_thread_num();
-        int local_n = n / thread_count;
-        int start = my_rank * local_n;
-        int end = (my_rank + 1) * local_n - 1;
-
-        if (my_rank == thread_count - 1)
-            end = n;
-
-        mpf_t term, elocal;
-        mpf_init2(term, 16777216);
-        mpf_init2(elocal, 16777216);
-        mpf_set_ui(term, 1);
-        mpf_set_ui(elocal, 0);
-
-        for (int i = 0; i < start; i++) {
-            mpf_div_ui(term, term, i + 1);
-        }
-
-        printf("\nEu sou a thread: %d e estou calculando de %d até %d", my_rank, start, end);
-
-        for (int i = start; i <= end; ++i) {
-            mpf_add(elocal, elocal, term);
-            mpf_div_ui(term, term, i + 1);
-        }
-
-        #pragma omp critical
-        mpf_add(*e, *e, elocal);
-
-        mpf_clear(term);
-        mpf_clear(elocal);
+        int start = (my_rank == 0) ? 0 : 1;
+        partial_sum(partial_results[my_rank], start, n, 2);
     }
+
+    mpf_add(*e, partial_results[0], partial_results[1]);
+
+    mpf_clear(partial_results[0]);
+    mpf_clear(partial_results[1]);
 }
 
-
+// Restante do código...
 
 
 void save_to_file(mpf_t e, const char *filename) {
@@ -57,7 +64,7 @@ void save_to_file(mpf_t e, const char *filename) {
 
 int main(int argc, char *argv[]) {
     int n = 0;
-    mpf_t e;
+    mpf_t e;;
     mpf_init2(e, 16777216);
     mpf_set_ui(e, 0);
     int thread_count = strtol(argv[1], NULL, 10);
